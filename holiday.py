ed18cd44ec439013c3ebcc36cedb99e412f505c2
Shawn Castrianni <Shawn.Castrianni@ihsmarkit.com>
2022-12-31 11:56:37 -0600

    Add holiday processing

--- holiday.py
+++ holiday.py
@@ -7,16 +7,10 @@ import datetime
 HOL_DATE_FORMAT = "%Y/%m/%d"
 ICS_DATE_FORMAT = "%Y%m%d"
 
-INPUT_TIMEZONE = "America/Los_Angeles"
-INPUT_TIME_EMPLOYEE_START = "0830"
-INPUT_TIME_EMPLOYEE_STOP = "0900"
-INPUT_TIME_SUPERVISOR_START = "1130"
-INPUT_TIME_SUPERVISOR_STOP = "1200"
-INPUT_BUSY_STATUS = "FREE"
-INPUT_ALARM = "-PT18H"
+INPUT_BUSY_STATUS = "OOF"
 
 
-def print_calendar(outfilename, section, event_lines, is_supervisor):
+def print_calendar(outfilename, section, event_lines):
     with open(outfilename, "wt") as outfile:
         outfile.write("BEGIN:VCALENDAR\n")
         outfile.write("VERSION:2.0\n")
@@ -26,29 +20,19 @@ def print_calendar(outfilename, section, event_lines, is_supervisor):
             event_tokens = line.split(",")
             event_name = event_tokens[0]
             event_date = event_tokens[1]
-            print_event(outfile, section, event_name, event_date, is_supervisor)
+            print_event(outfile, section, event_name, event_date)
         outfile.write("END:VCALENDAR\n")
 
 
-def print_event(outfile, section, name, date, is_supervisor):
-    time_start = INPUT_TIME_SUPERVISOR_START if is_supervisor else INPUT_TIME_EMPLOYEE_START
-    time_stop = INPUT_TIME_SUPERVISOR_STOP if is_supervisor else INPUT_TIME_EMPLOYEE_STOP
-    d = datetime.datetime.strptime(date, HOL_DATE_FORMAT)
+def print_event(outfile, section, name, date):
+    dstart = datetime.datetime.strptime(date, HOL_DATE_FORMAT)
+    dend = dstart + datetime.timedelta(days=1)
     outfile.write("BEGIN:VEVENT\n")
     outfile.write(f"SUMMARY:{name} ({section})\n")
-    outfile.write(f"DTSTART;TZID={INPUT_TIMEZONE}:{d.strftime(ICS_DATE_FORMAT)}T{time_start}\n")
-    outfile.write(f"DTEND;TZID={INPUT_TIMEZONE}:{d.strftime(ICS_DATE_FORMAT)}T{time_stop}\n")
+    outfile.write(f"DTSTART;VALUE=DATE:{dstart.strftime(ICS_DATE_FORMAT)}\n")
+    outfile.write(f"DTEND;VALUE=DATE:{dend.strftime(ICS_DATE_FORMAT)}\n")
     outfile.write(f"X-MICROSOFT-CDO-BUSYSTATUS:{INPUT_BUSY_STATUS}\n")
-    print_alarm(outfile)
     outfile.write("END:VEVENT\n")
-
-
-def print_alarm(outfile):
-    outfile.write("BEGIN:VALARM\n")
-    outfile.write("DESCRIPTION:REMINDER\n")
-    outfile.write(f"TRIGGER;RELATED=START:{INPUT_ALARM}\n")
-    outfile.write("ACTION:DISPLAY\n")
-    outfile.write("END:VALARM\n")
 
 
 def main():
@@ -63,8 +47,7 @@ def main():
     event_count = int(match.group("event_count"))
     if len(input_lines) != event_count + 1:
         raise Exception(f"Number of events {len(input_lines) - 1} does not match section header {event_count}")
-    print_calendar(f"{infile_path.stem}_employee.ics", section, input_lines[1:], False)
-    print_calendar(f"{infile_path.stem}_supervisor.ics", section, input_lines[1:], True)
+    print_calendar(f"{infile_path.stem}.ics", section, input_lines[1:])
 
 
 if __name__ == '__main__':
